f5e6f57238d1e3cdf1d4b8b0e2716f627636a482
Leila Schmidt <95034223+LeilaSchmidt@users.noreply.github.com>
2023-02-28 10:36:14 +0100

    Add project files.

---
+++ ConsoleApp1/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Inventory
+    {
+        public String name { get; set; }
+        public int count { get; set; }
+        public static int numberOfItemsCreated = 1;
+
+        public Inventory(String name, int count)
+        {
+            this.name = name;
+            this.count = count;
+            numberOfItemsCreated++;
+        }
+        public void displayTest()
+        {
+            Console.WriteLine("the item " + name + " with value " + count + " has been created");
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                List<Inventory> listOfObjs = new List<Inventory>();
+
+                Console.WriteLine("What item would you like to add to the inventory?");
+                string item = Console.ReadLine();
+                Console.WriteLine("How many of this item do you have?");
+                int count = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(Inventory.numberOfItemsCreated);
+
+                Inventory item1 = new Inventory(item, count);
+
+                item1.displayTest();
+
+                foreach (Inventory items in listOfObjs)
+                {
+                    Console.WriteLine("There are " + items.count);
+                }
+
+                Console.WriteLine("Do you wish to create another item?");
+                string cont = Console.ReadLine();
+                if (cont == "no")
+                {
+                    Console.WriteLine("The items you created were:");
+                    foreach (Inventory items in listOfObjs)
+                    {
+                        Console.WriteLine(items.count + " " + items.name);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
