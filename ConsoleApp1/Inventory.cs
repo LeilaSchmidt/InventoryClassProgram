fd4adede418f86901194f0823895924db12e66a6
Leila Schmidt <lnsnani80@gmail.com>
2023-03-01 21:02:15 +0100

    Updated the corresponding methods

--- ConsoleApp1/Inventory.cs
+++ ConsoleApp1/Inventory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -11,18 +11,55 @@ namespace ConsoleApp1
     {
         public String name { get; set; }
         public int count { get; set; }
-        public static int numberOfItemsCreated = 1;
 
         public Inventory(String name, int count)
         {
             this.name = name;
             this.count = count;
-            numberOfItemsCreated++;
         }
-        public void displayTest()
+        public Inventory(String name)
         {
-            Console.WriteLine("the item " + name + " with value " + count + " has been created");
+            this.name = name;
         }
+
+        public Inventory()
+        {
+            return;
+        }
+
+        public static void countInitialization(int index, List<Inventory> myList)
+        {
+
+            Console.WriteLine("How many of this item do you have?");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            myList[index].count = amount;
+            Console.WriteLine("Item: " + myList[index].name + ", now has a count of: " + myList[index].count);
+        }
+
+        public static void updateCount(List<Inventory> myList)
+        {
+            Console.WriteLine("\nwhich item would you like to update?");
+            var index = Console.ReadLine();
+            int intIndex = Convert.ToInt32(index);
+
+
+            Console.WriteLine("How many would you like to add or remove?");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            myList[intIndex].count += amount;
+            Console.WriteLine("Item: " + myList[intIndex].name + ", now has a count of: " + myList[intIndex].count);
+        }
+
+        public static void displayFinalOutput(List<Inventory> myList)
+        {
+            Console.WriteLine("\nThe items you created were:");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(i + ": " + myList[i].name + " has a count of: " + myList[i].count);
+            }
+        }
+
 
     }
 }
